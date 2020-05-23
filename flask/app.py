5325679cd95d99b04c7e8bd23a3c19567409760f
abhishekh <abhishekh2001@gmail.com>
2020-05-23 18:37:39 +0530

    models

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
 app = Flask(__name__)
+app.secret_key = 'test'  # app.secret_key = 'a26d9e7eff0d4dba88d35458034c57b1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, '../dbase.sqlite')
+
+
+# Import database and models
+from models import db, User
+db.create_all()
 
 
 @app.route('/')
 def homepage():
+    user = User(firstname='Abhishekh', lastname='Sivakumar', username='abhishekh', password='Test', email='abhishekh@gmail.com')
+    db.session.add(user)
+    db.session.commit()
+
+    print(User.query.all())
     return render_template("index.html");
 
 
