1d49cff4e1a5a048a02b73f501c5fd165329cedb
abhishekh <abhishekh2001@gmail.com>
2020-05-25 19:09:19 +0530

    tag feature

--- application/models.py
+++ application/models.py
@@ -4,21 +4,14 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash
 
 
-association_table = db.Table('association',
-                 db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-                 db.Column('newsarticle_id', db.Integer, db.ForeignKey('newsarticle.id'), primary_key=True)
-                 )
-
-#
-# class User(db.Model, UserMixin):
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     firstname = db.Column(db.String(24), nullable=False)
-#     lastname = db.Column(db.String(24))
-#     username = db.Column(db.String(24), unique=True, nullable=False)
-#     password = db.Column(db.String(128), nullable=False)
-#     email = db.Column(db.String(), unique=True, nullable=False)
-#
-#     saved_articles = db.relationship("NewsArticle", secondary=association_table)
+saved_articles = db.Table('association',
+                          db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+                          db.Column('newsarticle_id', db.Integer, db.ForeignKey('newsarticle.id'), primary_key=True)
+                          )
+related_tags = db.Table('related_tags',
+                        db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+                        db.Column('newsarticle_id', db.Integer, db.ForeignKey('newsarticle.id'), primary_key=True)
+                        )
 
 
 class User(db.Model, UserMixin):
@@ -29,8 +22,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(), unique=True, nullable=False)
 
-    saved_articles = db.relationship("NewsArticle", secondary=association_table,
-                                     backref=db.backref('newsarticles'))
+    saved_articles = db.relationship("NewsArticle", secondary=saved_articles,
+                                     backref=db.backref('users'))
 
 
 @event.listens_for(User.password, 'set', retval=True)
@@ -44,4 +37,16 @@ class NewsArticle(db.Model):
     __tablename__ = 'newsarticle'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     link = db.Column(db.String(200), nullable=False)
+    date_published = db.Column(db.DateTime, nullable=True)
+    related_tags = db.relationship("Tag", secondary=related_tags,
+                                   backref=db.backref('newsarticles'))
 
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    tag = db.Column(db.String(100), nullable=False)
+
+
+class TestDB(db.Model):
+    date_published = db.Column(db.DateTime, nullable=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 from flask import request, render_template, redirect, flash, url_for, session, g
 from flask import current_app as app
 from flask_login import login_required, logout_user, current_user, login_user
-from .models import db, User, NewsArticle
+from .models import db, User, NewsArticle, Tag
 from . import news_api_client
 from werkzeug.security import check_password_hash
 from . import login_manager
@@ -79,12 +79,6 @@ def logout():
     return redirect(url_for('login'))
 
 
-@app.route('/test')
-def test():
-    articles = User.query.filter_by(id=current_user.id).first().saved_articles
-    return render_template('test.html', articles=articles)
-
-
 @login_manager.user_loader
 def load_user(user_id):
     if user_id is not None:
@@ -98,13 +92,13 @@ def unauthorized():
     return redirect(url_for('login'))
 
 
+#  TESTING METHODS
 @app.route('/news_api_test')
 def news_tester():
     headlines = news_api_client.get_top_headlines(language='en')
     return render_template('news_tester.html', articles=headlines['articles'])
 
 
-# TESTING METHODS
 @app.route('/newstest', methods=('GET', 'POST'))
 def newstest():
     if request.method == 'POST':
@@ -150,4 +144,23 @@ def favorite_articles():
 @app.route('/display_articles')
 def display():
     news = NewsArticle.query.all()
-    return render_template('test/display_articles.html', articles=news)
+    tags = Tag.query.all()
+    return render_template('test/display_articles.html', articles=news, tags=tags)
+
+
+# DO NOT REMOVE THIS PLEASE
+@app.route('/test')
+def test():
+    for article in NewsArticle.query.all():
+        print('Link ' + str(article.link) + ' id ' + str(article.id), end='')
+        print(NewsArticle.query.filter_by(id=article.id).first().users)
+    #  Given the news_article id, retrieve the users that are linked to it
+    #  print(NewsArticle.query.filter_by(id=4).first().users)
+    #  Retrieve all saved articles by the user, given the ID
+    articles = User.query.filter_by(id=current_user.id).first().saved_articles
+    return render_template('test.html', articles=articles)
+
+
+@app.route('/generate_tags')
+def generate_tags():
+    return redirect(url_for('test'))
