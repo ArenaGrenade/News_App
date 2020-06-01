b57e3898565e9240031ce24fc72964285a155585
abhishekh <abhishekh2001@gmail.com>
2020-06-01 20:55:35 +0530

    heroku

--- config.py
+++ config.py
@@ -5,14 +5,11 @@ NEWS_API_KEY = '78a80e3fd33f4c35ba74643024219cfd'
 
 
 class Config:
-
     SECRET_KEY = 'b091yhcojm65ttn91vyc10cggynxmucyhcg0184cg08n1gc000gcn8gmyxg1yg48xg0874g7gcn8mgmxyg'
     FLASK_APP = 'app_runner.py'
     FLASK_DEBUG = 1
-    project_dir = os.path.dirname(os.path.abspath(__file__))
-    database_file = "sqlite:///{}".format(os.path.join(project_dir, "db.sqlite"))
-    SQLALCHEMY_DATABASE_URI = database_file
 
+    SQLALCHEMY_DATABASE_URI = "postgres://yzuauynghmyjvr:7258bd70210a5d656b72fe8fae518f961504e728ede3ca727658c8fdae12ac5d@ec2-54-211-210-149.compute-1.amazonaws.com:5432/daktevo0cgdf4v"
     print(basedir)
     #SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
